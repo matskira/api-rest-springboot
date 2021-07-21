a3f74f551cf046d551296694b2bac37aa87f0c66
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-21 14:10:40 -0300

    Criado primeira interface repository que extende nosso JPARepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,25 +1,28 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDTO> lista(){
-		Topico topico = new Topico("Duvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		Topico topicoDois = new Topico("Duvida Dois", "Dúvida com Java", new Curso("Java", "Programação"));
-		Topico topicoTres = new Topico("Duvida Três", "Dúvida com Angular", new Curso("Angular", "Front-end"));
 		
-		return TopicoDTO.converterTopico(Arrays.asList(topico, topicoDois, topicoTres));
+		List<Topico> topicos = topicoRepository.findAll();
+		
+		return TopicoDTO.converterTopico(topicos);
 		
 	}
 }
