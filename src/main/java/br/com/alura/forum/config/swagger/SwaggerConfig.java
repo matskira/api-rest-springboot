1a44f718a28692edbed07aa5c279b19899ad40c3
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-24 15:37:03 -0300

    Adicionado bibliotecas de swagger na aplicação e configurações inicias

---
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.config.swagger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+//Classe que ativa o Swagger na API
+@Configuration
+@EnableSwagger2 //Ativa o swagger
+public class SwaggerConfig {
+	
+	//Configurar aspectos dos endpoints expostos por ele
+	@Bean
+	public Docket api() {
+		/*
+		 * Através do apis e paths conseguimos enxergar todas as apis e paths que temos na aplicação
+		 * e através disso obtemos os endpoints 
+		 */
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build().useDefaultResponseMessages(false).globalResponseMessage(RequestMethod.GET, responseMessageForGet());
+	}
+	
+	//Define quais são as mensagens e status code que serão exibidos
+	private List<ResponseMessage> responseMessageForGet(){
+		return new ArrayList<ResponseMessage>() {{
+			add(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build());
+			add(new ResponseMessageBuilder().code(403).message("Forbidden!").build());
+		}};
+	}
+}
